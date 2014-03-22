0c47eefbcf010b8d08793b30fbd1b82c0bf356b2
Shashank Shekhar <shashankjet@gmail.com>
2014-03-22 15:13:24 +0530

    caching response at cron

--- scheduled_task.py
+++ scheduled_task.py
@@ -1,6 +1,7 @@
 import logging
 import redis
 import time
+import json
 from apscheduler.scheduler import Scheduler
 
 import process_tweet
@@ -12,6 +13,7 @@ logging.basicConfig()
 redis_client = redis.Redis(config.redis_server)
 
 redis_client.set('last_tweet_id', 444594459758452736)
+
 
 def run():
     search_results = twitter_wrapper.search(word=config.hashtag, since_id=int(redis_client.get('last_tweet_id')))
@@ -34,11 +36,55 @@ def run():
         redis_client.set('last_updated_at', int(time.time()))
     redis_client.set('last_tweet_id', search_results[0].id)
 
+def cache_response():
+    last_known_update = redis_client.hget('cache', 'last_updated')
+    if last_known_update:
+        last_known_update = int(last_known_update)
+    else:
+        last_known_update = 0
+
+    updated_at = int(redis_client.get('last_updated_at'))
+
+    if last_known_update<updated_at:
+
+        total_tweet_count = 0
+
+        top_usernames = redis_client.zrevrange('users', 0, 10)
+        top_users = [twitter_wrapper.get_user_dict(username) for username in top_usernames]
+        for user in top_users: #
+            user['top_words'] = list(redis_client.zrevrange('userwords_%s'%user['username'], 0, 5))
+            user['tweet_count'] = int(redis_client.zscore('users', user['username']))
+            total_tweet_count += user['tweet_count']
+
+        top_mention_usernames = redis_client.zrevrange('user_mentions', 0, 10)
+        top_mention_users = [twitter_wrapper.get_user_dict(username) for username in top_mention_usernames]
+        for user in top_mention_users: #
+            user['mention_count'] = int(redis_client.zscore('user_mentions', user['username']))
+
+        top_words = redis_client.zrevrange('words', 0, 10)
+        top_words = [dict([(word, int(redis_client.zscore('words', word)))]) for word in top_words]
+
+        response = json.dumps({
+                    'top_users':top_users,
+                    'top_mentions':top_mention_users,
+                    'top_keywords':top_words,
+                    'user_count':int(redis_client.zcard('users')),
+                    'mention_count':int(redis_client.zcard('user_mentions')),
+                    'keyword_count':int(redis_client.zcard('words')),
+                    'tweet_count':total_tweet_count,
+                    'updated_at': updated_at,
+                    'status': 1
+                    }, indent=4)
+
+        redis_client.hset('cache', 'last_updated', updated_at)
+        redis_client.hset('cache', 'response', response)
+
 
 scheduler = Scheduler()
 @scheduler.interval_schedule(seconds=200)
 def startJob():
     run()
+    cache_response()
 
 startJob()
 scheduler.start()
--- server.py
+++ server.py
@@ -2,7 +2,6 @@ import redis
 import json
 from flask import Flask, jsonify, request, make_response
 
-
 import config
 import twitter_wrapper
 
@@ -30,84 +29,9 @@ def home():
 
 @app.route('/rubyconfindia2014', methods=['GET'])
 def rubyconf():
-	last_known_update = redis_client.hget('cache', 'last_updated')
-	if last_known_update:
-		last_known_update = int(last_known_update)
-	else:
-		last_known_update = 0
-
-	updated_at = int(redis_client.get('last_updated_at'))
-
-	if last_known_update<updated_at:
-
-		total_tweet_count = 0
-
-		top_usernames = redis_client.zrevrange('users', 0, 10)
-		top_users = [twitter_wrapper.get_user_dict(username) for username in top_usernames]
-		for user in top_users: #
-			user['top_words'] = list(redis_client.zrevrange('userwords_%s'%user['username'], 0, 5))
-			user['tweet_count'] = int(redis_client.zscore('users', user['username']))
-			total_tweet_count += user['tweet_count']
-
-		top_mention_usernames = redis_client.zrevrange('user_mentions', 0, 10)
-		top_mention_users = [twitter_wrapper.get_user_dict(username) for username in top_mention_usernames]
-		for user in top_mention_users: #
-			user['mention_count'] = int(redis_client.zscore('user_mentions', user['username']))
-
-		top_words = redis_client.zrevrange('words', 0, 10)
-		top_words = [dict([(word, int(redis_client.zscore('words', word)))]) for word in top_words]
-
-		response = json.dumps({
-					'top_users':top_users,
-					'top_mentions':top_mention_users,
-					'top_keywords':top_words,
-					'user_count':int(redis_client.zcard('users')),
-					'mention_count':int(redis_client.zcard('user_mentions')),
-					'keyword_count':int(redis_client.zcard('words')),
-					'tweet_count':total_tweet_count,
-					'updated_at': updated_at,
-					'status': 1
-					}, indent=4)
-
-		redis_client.hset('cache', 'last_updated', updated_at)
-		redis_client.hset('cache', 'response', response)
-
-	else:
-		response = redis_client.hget('cache', 'response')
+	response = redis_client.hget('cache', 'response')
 
 	return jsonify(json.loads(response))
-
-@app.route('/rubyconfindia2014/word/<word>', methods=['GET'])
-def rubyconf(word):
-	try:
-		if request.args.get('since_serialid'):
-			since_id = int(request.args.get('since_serialid'))
-		elif request.args.get('max_serialid'):
-			max_id = int(request.args.get('max_serialid'))
-
-		if request.args.get('count'):
-			count = int(request.args.get('count'))
-			count = count if count<101 else 100
-		else:
-			count = 30
-
-	except ValueError:
-		return make_response(jsonify({'status':0, 'message':'Wrong Value for Parameters'}))
-	
-	if redis_client.exists('wordtweets_%s'%word):
-		max_id = max_id if max_id else count
-
-		since_id = since_id if since_id else -1
-
-		redis_client.lrange('wordtweets_%s'%word, , )
-
-
-
-
-		
-
-	return jsonify(json.loads(response))
-
 
 if __name__ == '__main__':
 	app.run('0.0.0.0', 80)
