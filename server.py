af59efff69cf863d7847935f2d2ca0a3d8274aa1
Shashank Shekhar <shashankjet@gmail.com>
2014-03-23 10:23:07 +0530

    added heading and other shebangs to template

--- server.py
+++ server.py
@@ -30,8 +30,8 @@ def rubyconftest():
 	response = redis_client.hget('cache', 'response')
 	response = json.loads(response)
 	ctop_keywords = [['keywords', 'frequence of occurence']] + [[item['word'], item['count']] for item in response['top_keywords']]
-	ctop_mentions = [['users', 'Number of times mentioned']] + ['@%s'%[item['username'], item['mention_count']] for item in response['top_mentions']]
-	ctop_users = [['users', 'Number of tweets']] + ['@%s'%[item['username'], item['tweet_count']] for item in response['top_users']]
+	ctop_mentions = [['users', 'Number of times mentioned']] + [['@%s'%item['username'], item['mention_count']] for item in response['top_mentions']]
+	ctop_users = [['users', 'Number of tweets']] + [['@%s'%item['username'], item['tweet_count']] for item in response['top_users']]
 	return render_template('charts.html',
 							ctop_keywords = json.dumps(ctop_keywords),
 							ctop_mentions = json.dumps(ctop_mentions),
