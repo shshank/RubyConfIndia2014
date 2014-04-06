72de79af65c58928468b839c2ade0f43ea49dc3f
Shashank Shekhar <shashankjet@gmail.com>
2014-04-07 01:47:26 +0530

    ignored more words for yuvi

--- config.py
+++ config.py
@@ -2,7 +2,9 @@ hashtag = '#miracleboyyuvi'
 
 redis_server = 'localhost'
 
-ignore_words = ['yuvraj', 'singh', 'rt', 'url',
+ignore_words = ['yuvraj', 'singh', 'rt', 'url', 'wt20final', 'indvssl2014', 'indvssl',
 				hashtag, hashtag.replace('#', '')]
+
+#join_words = [('vijay', 'mallya', 'vijay mallya'), ('world', 'cup')]
 
 first_tweet_id = 444594459758452736
